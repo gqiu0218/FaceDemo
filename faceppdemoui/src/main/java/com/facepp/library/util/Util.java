ccd57b660c013b11f3f5fa22bb4cd2f9c8d67a24
gqiu0218 <107675726+8>
2017-11-19 23:08:11 +0800

    1.认证通过

---
+++ faceppdemoui/src/main/java/com/facepp/library/util/Util.java
@@ -0,0 +1,9 @@
+package com.facepp.library.util;
+
+public class Util {
+
+    //在这边填写 API_KEY 和 API_SECRET
+    public static String API_KEY = "lrdc-6E_A2qhktywPLQujEKpexdrH1_I";
+    public static String API_SECRET = "A8YkzHpEkWaGPgsuWfxy9ZzI1OIWNP4k";
+
+}
